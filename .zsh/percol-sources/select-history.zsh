6337bf631cae6a0ce0a367e1aefd19298b98c198
kotamat <kota1861@gmail.com>
2017-11-28 16:41:34 +0900

    update

--- .zsh/percol-sources/select-history.zsh
+++ .zsh/percol-sources/select-history.zsh
@@ -1,8 +1,8 @@
 function percol-select-history() {
   local tac_cmd
   which gtac &> /dev/null && tac_cmd=gtac || tac_cmd=tac
-  BUFFER=$($tac_cmd ~/.zsh_history | sed 's/^: [0-9]*:[0-9]*;//' \
-    | percol --match-method regex --query "$LBUFFER")
+  BUFFER=$($tac_cmd ~/.zsh_history | LC_ALL=C sed 's/^: [0-9]*:[0-9]*;//' \
+    | peco)
   CURSOR=$#BUFFER         # move cursor
   zle -R -c               # refresh
 }
